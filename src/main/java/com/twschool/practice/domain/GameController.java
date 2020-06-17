17f94cec6dd343384d7244f015989acbeaeaa5c0
306093226wanggitgitgitgit <306093226@qq.com>
2020-06-17 21:25:45 +0800

    change GameController.java

--- src/main/java/com/twschool/practice/domain/GameController.java
+++ src/main/java/com/twschool/practice/domain/GameController.java
@@ -1,4 +1,24 @@
 package com.twschool.practice.domain;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class GameController {
+
+    @GetMapping("/game")
+    public Map<String, String> guess(@RequestParam String guess) {
+        Map<String, String> map = new HashMap<String, String>();
+        GuessNumberGame guessNumberGame = new GuessNumberGame(new RandomAnswerGenerator());
+        String result = guessNumberGame.guess(Arrays.asList(guess.split(" ")));
+        map.put("input", guess);
+        map.put("result", result);
+        return map;
+    }
+
 }
