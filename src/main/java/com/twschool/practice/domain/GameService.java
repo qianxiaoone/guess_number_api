c09cb3bc68f861443caa38b1c9695a2428b819a8
306093226wanggitgitgitgit <306093226@qq.com>
2020-06-17 22:53:08 +0800

    change GameService.java and test

--- src/main/java/com/twschool/practice/domain/GameService.java
+++ src/main/java/com/twschool/practice/domain/GameService.java
@@ -10,8 +10,24 @@ public class GameService {
         this.guessNumberGame = guessNumberGame;
     }
 
+    public GameService() {
+
+    }
+
     public String guess(String userAnswerString) {
         List<String> userAnswer = Arrays.asList(userAnswerString.split(" "));
+        GuessNumberGame guessNumberGame = getGame();
         return guessNumberGame.guess(userAnswer);
+    }
+
+    public GuessNumberGame getGame() {
+        if (guessNumberGame == null) {
+            guessNumberGame = createGuessNumberGame();
+        }
+        return guessNumberGame;
+    }
+
+    public GuessNumberGame createGuessNumberGame() {
+        return new GuessNumberGame(new RandomAnswerGenerator());
     }
 }
--- src/test/java/com/twschool/practice/domain/GameServiceTest.java
+++ src/test/java/com/twschool/practice/domain/GameServiceTest.java
@@ -1,4 +1,22 @@
 package com.twschool.practice.domain;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+
 public class GameServiceTest {
+    @Test
+    public void should_create_game_once_when_guess_incorrect_number_twice(){
+        GameService gameService = new GameService();
+        GameService spyGameService = Mockito.spy(gameService);
+
+        RandomAnswerGenerator randomAnswerGenerator =Mockito.mock(RandomAnswerGenerator.class);
+        Mockito.when(randomAnswerGenerator.generateAnswer()).thenReturn(new Answer(Arrays.asList("1","2","3","4")));
+        Mockito.doReturn(new GuessNumberGame(randomAnswerGenerator)).when(spyGameService).createGuessNumberGame();
+        spyGameService.guess("1 2 3 5");
+        spyGameService.guess("1 2 3 5");
+        Mockito.verify(spyGameService,Mockito.times(1)).createGuessNumberGame();
+
+    }
 }
